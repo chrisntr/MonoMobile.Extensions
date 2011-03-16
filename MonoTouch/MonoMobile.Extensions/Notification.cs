af95834be3db167f04b3caadbb8fb7820e2bcf40
chrisntr <chrisntr@gmail.com>
2011-03-16 02:07:16 +0000

    Added in support for beep and vibrate, need to do an example for both. Completed Alert and Confirm and updated example.

--- MonoTouch/MonoMobile.Extensions/Notification.cs
+++ MonoTouch/MonoMobile.Extensions/Notification.cs
@@ -1,5 +1,6 @@
 using System;
 using MonoTouch.UIKit;
+using MonoTouch.AudioToolbox;
 namespace MonoMobile.Extensions
 {
 	public class Notification
@@ -8,65 +9,73 @@ namespace MonoMobile.Extensions
 		{
 		}
 	
-		public void Alert (string message, Action<int> alertCallback)
+		public void Alert (string message, Action alertCallback)
 		{
 			Alert(message, alertCallback, "Alert", "OK");
 		}
 
-		public void Alert (string message, Action<int> alertCallback, string title)
+		public void Alert (string message, Action alertCallback, string title)
 		{
             Alert(message, alertCallback, title, "OK");
 		}
 
-		public void Alert (string message, Action<int> alertCallback, string title, string buttonName)
+		public void Alert (string message, Action alertCallback, string title, string buttonName)
+		{
+			using(var alertView = new UIAlertView(title, message, null, buttonName, null))
+			{
+				alertView.Show();
+				alertView.Dismissed += delegate(object sender, UIButtonEventArgs e) {
+					alertCallback();
+				};
+			}
+		}
+
+		public void Confirm (string message, Action<int> confirmCallback)
+		{
+			
+			Confirm(message, confirmCallback, "Alert", "OK");
+		}
+
+		public void Confirm (string message, Action<int> confirmCallback, string title)
+		{
+			Confirm(message, confirmCallback, title, "OK");
+		}
+
+		public void Confirm (string message, Action<int> confirmCallback, string title, string buttonLabels)
 		{
 			using(var alertView = new UIAlertView(title, message, null, null, null))
 			{
-				var labels = buttonName.Split(new Char[]{','});	
+				var labels = buttonLabels.Split(new Char[]{','});	
 				foreach(var label in labels)
 				{
 					alertView.AddButton(label);
 				}
 				alertView.Show();
 				alertView.Clicked += delegate(object sender, UIButtonEventArgs e) {
-					alertCallback(e.ButtonIndex);
+					confirmCallback(e.ButtonIndex);
 				};
 			}
 		}
 
-		public void Confirm (string message, Action confirmCallback)
-		{
-			throw new NotImplementedException ();
-		}
-
-		public void Confirm (string message, Action confirmCallback, string title)
-		{
-			throw new NotImplementedException ();
-		}
-
-		public void Confirm (string message, Action confirmCallback, string title, string buttonLabels)
-		{
-			throw new NotImplementedException ();
-		}
-
 		public void Beep ()
 		{
-			throw new NotImplementedException ();
+			var beep = SystemSound.FromFile("beep.wav");
+			beep.PlaySystemSound();
 		}
 
 		public void Beep (int times)
 		{
-			throw new NotImplementedException ();
+			Beep();
 		}
 
 		public void Vibrate ()
 		{
-			throw new NotImplementedException ();
+			SystemSound.Vibrate.PlaySystemSound();
 		}
 
 		public void Vibrate (int milliseconds)
 		{
-			throw new NotImplementedException ();
+			Vibrate();
 		}
 	}
 }
