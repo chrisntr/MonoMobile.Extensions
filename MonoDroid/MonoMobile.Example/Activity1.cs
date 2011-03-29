b2a3b34cbf3a199cdcdf8fb0a27620c0b47b0eb9
Anders Hammervold <anders@hammervold.com>
2011-03-29 22:02:55 +0200

    First implementation of IGeolocation

--- MonoDroid/MonoMobile.Example/Activity1.cs
+++ MonoDroid/MonoMobile.Example/Activity1.cs
@@ -2,39 +2,58 @@ using System;
 
 using Android.App;
 using Android.Content;
+using Android.Locations;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using MonoMobile.Extensions;
 
 namespace MonoMobile.Example
 {
 	[Activity(Label = "MonoMobile Android Example", MainLauncher = true)]
 	public class Activity1 : Activity
 	{
-		int count = 1;
-
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 			
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
-			
+
+		    LocationManager locationManager=(LocationManager) GetSystemService(LocationService);
+            IGeolocation location = new Geolocation(locationManager);
+		    location.WatchPosition(WatchSuccess);
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button> (Resource.Id.MyButton);
 			
-			button.Click += delegate {
-				var device = new MonoMobile.Extensions.Device(this);
-				
-				Android.Util.Log.Info("MonoMobile.Extensions", "Device Name: {0}", device.Name);
-				Android.Util.Log.Info("MonoMobile.Extensions", "Device Platform: {0}", device.Platform);
-				Android.Util.Log.Info("MonoMobile.Extensions", "Device UUID: {0}", device.UUID);
-				Android.Util.Log.Info("MonoMobile.Extensions", "Device Version: {0}", device.Version);
-				Android.Util.Log.Info("MonoMobile.Extensions", "MonoMobile Version: {0}", device.MonoMobileVersion);
-			};
+			button.Click += delegate
+			                    {
+			                        LogDeviceInfo();
+			                        location.GetCurrentPosition(CurrentPositionSuccess);
+			                    };
 		}
+
+	    private void LogDeviceInfo()
+	    {
+	        var device = new MonoMobile.Extensions.Device(this);
+	        Android.Util.Log.Info("MonoMobile.Extensions", "Device Name: {0}", device.Name);
+	        Android.Util.Log.Info("MonoMobile.Extensions", "Device Platform: {0}", device.Platform);
+	        Android.Util.Log.Info("MonoMobile.Extensions", "Device UUID: {0}", device.UUID);
+	        Android.Util.Log.Info("MonoMobile.Extensions", "Device Version: {0}", device.Version);
+	        Android.Util.Log.Info("MonoMobile.Extensions", "MonoMobile Version: {0}", device.MonoMobileVersion);
+	    }
+
+	    private void CurrentPositionSuccess(Position obj)
+	    {
+            Android.Util.Log.Info("MonoMobile.Extensions", "GetCurrentPosition location: {0}-{1} [{2}]", obj.Coords.Latitude, obj.Coords.Longitude, obj.Coords.Accuracy);
+	    }
+
+	    private void WatchSuccess(Position obj)
+	    {
+            Android.Util.Log.Info("MonoMobile.Extension", "WatchPosition location: {0}-{1} [{2}]", obj.Coords.Latitude, obj.Coords.Longitude, obj.Coords.Accuracy);
+	    }
 	}
 }
 
---
+++ MonoDroid/MonoMobile.Extensions/Geolocation.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Linq;
+using Android.Locations;
+using Android.OS;
+using Android.Util;
+using Java.IO;
+
+namespace MonoMobile.Extensions
+{
+   
+    public class Geolocation : IGeolocation, ILocationListener
+    {
+        private readonly LocationManager _locationManager;
+
+        private Action<Position> _success;
+        private Action<PositionError> _error;
+        private GeolocationOptions _options;
+        private string _watchId;
+        private IntPtr _handle;
+
+        public Geolocation(LocationManager locationManager)
+        {
+            _locationManager = locationManager;
+            _success = position => { };
+            _error = error => { };
+            _options= new GeolocationOptions();
+            _watchId = Guid.NewGuid().ToString();
+            _handle = new IntPtr();
+        }
+
+        #region IGeolocation Members
+
+        public void GetCurrentPosition(Action<Position> success)
+        {
+            GetCurrentPosition(success, (error)=> { });
+        }
+
+        public void GetCurrentPosition(Action<Position> success, Action<PositionError> error)
+        {
+            GetCurrentPosition(success, error, new GeolocationOptions());
+        }
+
+        public void GetCurrentPosition(Action<Position> success, Action<PositionError> error, GeolocationOptions options)
+        {
+            var criteria = new Criteria {Accuracy = options.EnableHighAccuracy ? Accuracy.Fine : Accuracy.Coarse};
+            string bestProvider = _locationManager.GetBestProvider(criteria, true);
+            Location lastKnownLocation=null;
+            try
+            {
+                lastKnownLocation = _locationManager.GetLastKnownLocation(bestProvider);
+            }
+            catch (Exception exception)
+            {
+                error(new PositionError(PositionErrorCode.PositionUnavailable, exception.Message));
+            }
+            if (lastKnownLocation == null)
+            {
+                error(new PositionError(PositionErrorCode.PositionUnavailable,"No position aquired"));
+            }
+
+            SendLocation(lastKnownLocation, success);
+            
+        }
+
+        public string WatchPosition(Action<Position> success)
+        {
+            return WatchPosition(success, error => { });
+        }
+
+        public string WatchPosition(Action<Position> success, Action<PositionError> error)
+        {
+            return WatchPosition(success,error,new GeolocationOptions());
+        }
+
+        public string WatchPosition(Action<Position> success, Action<PositionError> error, GeolocationOptions options)
+        {
+            _success = success;
+            _error = error;
+            _options = options;
+            string provider = _options.EnableHighAccuracy ? LocationManager.GpsProvider : LocationManager.NetworkProvider;
+            
+            //TODO: set this value based on GeolocationOptions?
+            int minTime = 3000;
+
+            _locationManager.RequestLocationUpdates(provider,minTime, 0,this);
+
+            return _watchId;
+        }
+
+        public void ClearWatch(string watchID)
+        {
+            if (watchID != _watchId)
+                return;
+
+            _locationManager.RemoveUpdates(this);
+        }
+
+        #endregion
+
+        #region ILocationListener Members
+
+        public void OnLocationChanged(Location location)
+        {
+            SendLocation(location,_success);
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            //Dows this concern us ?
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            //Dows this concern us ?
+        }
+
+        public void OnStatusChanged(string provider, int status, Bundle extras)
+        {
+            //Dows this concern us ?
+        }
+
+        /// <summary>
+        /// Gets the JNI value of the underlying Android object.
+        /// </summary>
+        /// <value>
+        /// A <see cref="T:System.IntPtr"/> containing the JNI handle for an
+        ///           object instance within the Java VM.
+        /// </value>
+        /// <remarks/>
+        public IntPtr Handle
+        {
+            get { return _handle; }
+        }
+
+        #endregion
+
+        private void SendLocation(Location lastKnownLocation, Action<Position> success)
+        {
+            var position = new Position
+                               {
+                                   Timestamp = DateTime.Now,
+                                   Coords = new Coordinates
+                                                {
+                                                    Accuracy = lastKnownLocation.Accuracy,
+                                                    Heading = lastKnownLocation.Bearing,
+                                                    Altitude = lastKnownLocation.Altitude,
+                                                    AltitudeAccuracy = lastKnownLocation.Accuracy,
+                                                    Latitude = lastKnownLocation.Latitude,
+                                                    Longitude = lastKnownLocation.Longitude,
+                                                    Speed = lastKnownLocation.Speed
+                                                }
+                               };
+            success(position);
+        }
+    }
+}
