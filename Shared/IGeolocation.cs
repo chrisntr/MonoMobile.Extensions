a0ea421a941e046e96582b8d4ec9a3b669527542
ermau <me@ermau.com>
2011-11-01 10:45:21 -0400

    Future based current position on the contract

--- Shared/IGeolocation.cs
+++ Shared/IGeolocation.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace MonoMobile.Extensions
 {
     public interface IGeolocation
     {
-        void GetCurrentPosition(Action<Position> success);
-        void GetCurrentPosition(Action<Position> success, Action<PositionError> error);
-        void GetCurrentPosition(Action<Position> success, Action<PositionError> error, GeolocationOptions options);
+		/// <summary>
+		/// Gets a value indicating whether this <see cref="MonoMobile.Extensions.IGeolocation"/> supports heading.
+		/// </summary>
+		/// <value>
+		/// <c>true</c> if supports heading; otherwise, <c>false</c>.
+		/// </value>
+		bool SupportsHeading { get; }
+
+		/// <summary>
+		/// Gets a value indicating whether geolocation services are available.
+		/// </summary>
+		/// <value>
+		/// <c>true</c> if geolocation services are available; otherwise, <c>false</c>.
+		/// </value>
+		bool IsGeolocationAvailable { get; }
+
+		/// <summary>
+		/// Gets a future for the current position.
+		/// </summary>
+		/// <returns>
+		/// A <see cref="Task{TResult}"/> for the current position.
+		/// </returns>
+		/// <remarks>
+		/// If the the underlying OS needs to request location access permissions, it will occur automatically.
+		/// If the user has disallowed location access (now or previously), the future will be canceled.
+		/// Any errors currently sets an exception on the future, but this needs to be reviewed as
+		/// not all geolocation errors may be really be exceptional.
+		/// </remarks>
+		Task<Position> GetCurrentPosition();
+
+		/// <summary>
+		/// Gets a future for the current position.
+		/// </summary>
+		/// <param name="options">Options for getting the current position, currently ignored.</param>
+		/// <returns>
+		/// A <see cref="Task{TResult}"/> for the current position.
+		/// </returns>
+		/// <remarks>
+		/// If the the underlying OS needs to request location access permissions, it will occur automatically.
+		/// If the user has disallowed location access (now or previously), the future will be canceled.
+		/// Any errors currently sets an exception on the future, but this needs to be reviewed as
+		/// not all geolocation errors may be really be exceptional.
+		/// </remarks>
+		Task<Position> GetCurrentPosition (GeolocationOptions options);
 
         string WatchPosition(Action<Position> success);
         string WatchPosition(Action<Position> success, Action<PositionError> error);
@@ -43,6 +85,20 @@ namespace MonoMobile.Extensions
         public double Heading { get; set; }
         public double Speed { get; set; }
     }
+	
+	public class GeolocationException
+		: Exception
+	{
+		public GeolocationException()
+			: base()
+		{
+		}
+		
+		public GeolocationException (string message)
+			: base (message)
+		{
+		}
+	}
 
     public class PositionError
     {
