fe677408f35847ba8c9ac81bffc35b96f239681f
ermau <me@ermau.com>
2011-11-08 16:35:15 -0500

    Remove buttons for uncompleted features in iOS ex.

--- MonoTouch/MonoTouch.Example/ExampleList.xib.cs
+++ MonoTouch/MonoTouch.Example/ExampleList.xib.cs
@@ -50,59 +50,59 @@ namespace MonoTouch.Example
 		{
 			base.ViewDidLoad ();
 			
-			var notification = new Notification();
-			alertButton = UIButton.FromType(UIButtonType.RoundedRect);
-			alertButton.Frame = new System.Drawing.RectangleF(40f, 20f, 200f, 40f);
-			alertButton.SetTitle("Alert button", UIControlState.Normal);
-			alertButton.TouchUpInside += (s, e) => {
-				notification.Alert("My Message", () => {
-					Console.WriteLine ("Dismissed");
-				}, "Title", "OK");
-			};
-			this.View.AddSubview(alertButton);
-			
-			confirmButton = UIButton.FromType(UIButtonType.RoundedRect);
-			confirmButton.Frame = new System.Drawing.RectangleF(40f, 60f, 200f, 40f);
-			confirmButton.SetTitle("Confirm button", UIControlState.Normal);
-			confirmButton.TouchUpInside += (s, e) => {
-				notification.Confirm("My Message", (i) => {
-					Console.WriteLine ("Button {0} pressed", i);	
-				}, "Alert!", "One, Two, Cancelled");
-			};
-			this.View.AddSubview(confirmButton);
-			
-			beepButton = UIButton.FromType(UIButtonType.RoundedRect);
-			beepButton.Frame = new System.Drawing.RectangleF(40f, 100f, 200f, 40f);
-			beepButton.SetTitle("Beep!", UIControlState.Normal);
-			beepButton.TouchUpInside += (s, e) => {
-				// Beep overload just calls beep anyway due to iPhone limitation.
-				// Make sure there's a beep.wav set as content in the root of the app.
-				notification.Beep();
-			};
-			this.View.AddSubview(beepButton);
-			
-			
-			vibrateButton = UIButton.FromType(UIButtonType.RoundedRect);
-			vibrateButton.Frame = new System.Drawing.RectangleF(40f, 140f, 200f, 40f);
-			vibrateButton.SetTitle("Vibrate!", UIControlState.Normal);
-			vibrateButton.TouchUpInside += (s, e) => {
-				// Vibrate overload just calls vibrate anyway due to iPhone limitation.
-				notification.Vibrate();
-			};
-			this.View.AddSubview(vibrateButton);
-			
-			var camera = new Camera(this);
-			cameraButton = UIButton.FromType(UIButtonType.RoundedRect);
-			cameraButton.Frame = new System.Drawing.RectangleF(40f, 180f, 200f, 40f);
-			cameraButton.SetTitle("Get Picture!", UIControlState.Normal);
-			cameraButton.TouchUpInside += (s, e) => {
-				// Vibrate overload just calls vibrate anyway due to iPhone limitation.
-				camera.GetPicture(new Camera.CameraOptions() { SourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum }, 
-					(p) => { Console.WriteLine ("Got picture as {0}", p);},
-					() => { Console.WriteLine ("Cancelled"); }
-				);
-			};
-			this.View.AddSubview(cameraButton);
+//			var notification = new Notification();
+//			alertButton = UIButton.FromType(UIButtonType.RoundedRect);
+//			alertButton.Frame = new System.Drawing.RectangleF(40f, 20f, 200f, 40f);
+//			alertButton.SetTitle("Alert button", UIControlState.Normal);
+//			alertButton.TouchUpInside += (s, e) => {
+//				notification.Alert("My Message", () => {
+//					Console.WriteLine ("Dismissed");
+//				}, "Title", "OK");
+//			};
+//			this.View.AddSubview(alertButton);
+//			
+//			confirmButton = UIButton.FromType(UIButtonType.RoundedRect);
+//			confirmButton.Frame = new System.Drawing.RectangleF(40f, 60f, 200f, 40f);
+//			confirmButton.SetTitle("Confirm button", UIControlState.Normal);
+//			confirmButton.TouchUpInside += (s, e) => {
+//				notification.Confirm("My Message", (i) => {
+//					Console.WriteLine ("Button {0} pressed", i);	
+//				}, "Alert!", "One, Two, Cancelled");
+//			};
+//			this.View.AddSubview(confirmButton);
+//			
+//			beepButton = UIButton.FromType(UIButtonType.RoundedRect);
+//			beepButton.Frame = new System.Drawing.RectangleF(40f, 100f, 200f, 40f);
+//			beepButton.SetTitle("Beep!", UIControlState.Normal);
+//			beepButton.TouchUpInside += (s, e) => {
+//				// Beep overload just calls beep anyway due to iPhone limitation.
+//				// Make sure there's a beep.wav set as content in the root of the app.
+//				notification.Beep();
+//			};
+//			this.View.AddSubview(beepButton);
+//			
+//			
+//			vibrateButton = UIButton.FromType(UIButtonType.RoundedRect);
+//			vibrateButton.Frame = new System.Drawing.RectangleF(40f, 140f, 200f, 40f);
+//			vibrateButton.SetTitle("Vibrate!", UIControlState.Normal);
+//			vibrateButton.TouchUpInside += (s, e) => {
+//				// Vibrate overload just calls vibrate anyway due to iPhone limitation.
+//				notification.Vibrate();
+//			};
+//			this.View.AddSubview(vibrateButton);
+//			
+//			var camera = new Camera(this);
+//			cameraButton = UIButton.FromType(UIButtonType.RoundedRect);
+//			cameraButton.Frame = new System.Drawing.RectangleF(40f, 180f, 200f, 40f);
+//			cameraButton.SetTitle("Get Picture!", UIControlState.Normal);
+//			cameraButton.TouchUpInside += (s, e) => {
+//				// Vibrate overload just calls vibrate anyway due to iPhone limitation.
+//				camera.GetPicture(new Camera.CameraOptions() { SourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum }, 
+//					(p) => { Console.WriteLine ("Got picture as {0}", p);},
+//					() => { Console.WriteLine ("Cancelled"); }
+//				);
+//			};
+//			this.View.AddSubview(cameraButton);
 
 			locator = new Geolocation();
 			currentLocationButton = UIButton.FromType (UIButtonType.RoundedRect);
