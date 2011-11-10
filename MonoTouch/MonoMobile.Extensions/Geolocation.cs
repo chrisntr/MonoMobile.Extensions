b7556528028e2c739cdda2d6913ad0d2145c7d67
ermau <me@ermau.com>
2011-11-10 13:56:24 -0500

    Move docs, remove IGeolocation

--- MonoTouch/MonoMobile.Extensions/Geolocation.cs
+++ MonoTouch/MonoMobile.Extensions/Geolocation.cs
@@ -7,7 +7,6 @@ using MonoTouch.Foundation;
 namespace Xamarin.Geolocation
 {
 	public class Geolocation
-		: IGeolocation
 	{
 		public Geolocation()
 		{
@@ -18,45 +17,157 @@ namespace Xamarin.Geolocation
 			this.manager.UpdatedHeading += OnUpdatedHeading;
 		}
 		
+		/// <summary>
+		/// Raised when there is an error retrieving position information.
+		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// Temporary errors will not be reported.
+		/// </para>
+		/// <para>
+		/// If an error occurs, listening will be stopped automatically.
+		/// </para>
+		/// </remarks>
+		/// <seealso cref="PositionErrorCode"/>
 		public event EventHandler<PositionErrorEventArgs> PositionError;
+
+		/// <summary>
+		/// Raised when position information is updated.
+		/// </summary>
+		/// <seealso cref="Position"/>
 		public event EventHandler<PositionEventArgs> PositionChanged;
 		
+		/// <summary>
+		/// Gets or sets the desired accuracy in meters.
+		/// </summary>
 		public double DesiredAccuracy
 		{
 			get;
 			set;
 		}
 
+		/// <summary>
+		/// Gets whether the position is currently being listened to.
+		/// </summary>
 		public bool IsListening
 		{
 			get { return this.isListening; }
 		}
-		
+
+		/// <summary>
+		/// Gets a value indicating whether this <see cref="MonoMobile.Extensions.IGeolocation"/> supports heading.
+		/// </summary>
+		/// <value>
+		/// <c>true</c> if supports heading; otherwise, <c>false</c>.
+		/// </value>
 		public bool SupportsHeading
 		{
 			get { return CLLocationManager.HeadingAvailable; }
 		}
 
+		/// <summary>
+		/// Gets a value indicating whether geolocation services are available on the device.
+		/// </summary>
+		/// <value>
+		/// <c>true</c> if geolocation services are available; otherwise, <c>false</c>.
+		/// </value>
 		public bool IsGeolocationAvailable
 		{
-			get { return true; } // all devices support at least wifi geolocation
+			get { return true; } // all iOS devices support at least wifi geolocation
 		}
 
+		/// <summary>
+		/// Gets a value indicating whether geolocation is available and enabled.
+		/// </summary>
+		/// <value>
+		/// <c>true</c> if geolocation is available and enabled; otherwise, <c>false</c>.
+		/// </value>
 		public bool IsGeolocationEnabled
 		{
 			get { return CLLocationManager.LocationServicesEnabled; }
 		}
-		
+
+		/// <summary>
+		/// Gets a future for the current position.
+		/// </summary>
+		/// <param name="timeout">
+		/// The time before the request should be automatically cancelled in milliseconds. <see cref="Timeout.Infinite"/> for no timeout.
+		/// </param>
+		/// <returns>
+		/// A <see cref="Task{TResult}"/> for the current position.
+		/// </returns>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="timeout"/> is &lt= 0 and not <see cref="Timeout.Infinite"/>.</exception>
+		/// <remarks>
+		/// <para>
+		/// If the underlying OS needs to request location access permissions, it will occur automatically.
+		/// If the user has disallowed location access (now or previously), the future will be canceled.
+		/// </para>
+		/// <para>
+		/// The first position that is within the <see cref="DesiredAccuracy"/> will be returned. If no
+		/// position matches that accuracy, once <paramref cref="timeout"/> is reached, the most accurate
+		/// position will be returned. If the <paramref name="timeout"/> is reached with no position being
+		/// acquired, the task will be canceled.
+		/// </para>
+		/// <para>
+		/// If this <see cref="IGeolocation"/> currently <see cref="IsListening"/>, the future will be
+		/// set immediately with the last retrieved position.
+		/// </para>
+		/// </remarks>
 		public Task<Position> GetCurrentPosition (int timeout)
 		{
 			return GetCurrentPosition (timeout, CancellationToken.None);
 		}
-		
+
+		/// <summary>
+		/// Gets a future for the current position.
+		/// </summary>
+		/// <param name="cancelToken">A <see cref="CancellationToken"/> to cancel the position request.</param>
+		/// <returns>
+		/// A <see cref="Task{TResult}"/> for the current position.
+		/// </returns>
+		/// <remarks>
+		/// <para>
+		/// If the underlying OS needs to request location access permissions, it will occur automatically.
+		/// If the user has disallowed location access (now or previously), the future will be canceled.
+		/// </para>
+		/// <para>
+		/// The first position that is within the <see cref="DesiredAccuracy"/> will be returned.
+		/// </para>
+		/// <para>
+		/// If this <see cref="IGeolocation"/> currently <see cref="IsListening"/>, the future will be
+		/// set immediately with the last retrieved position.
+		/// </para>
+		/// </remarks>
 		public Task<Position> GetCurrentPosition (CancellationToken cancelToken)
 		{
 			return GetCurrentPosition (Timeout.Infinite, cancelToken);
 		}
 
+		/// <summary>
+		/// Gets a future for the current position.
+		/// </summary>
+		/// <param name="timeout">The time before the request should be automatically cancelled in milliseconds</param>
+		/// <param name="cancelToken">A <see cref="CancellationToken"/> to cancel the position request.</param>
+		/// <returns>
+		/// A <see cref="Task{TResult}"/> for the current position.
+		/// </returns>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="timeout"/> is &lt= 0 and not <see cref="Timeout.Infinite"/>.</exception>
+		/// <remarks>
+		/// <para>
+		/// If the underlying OS needs to request location access permissions, it will occur automatically.
+		/// If the user has disallowed location access (now or previously), the future will be canceled.
+		/// </para>
+		/// <para>
+		/// The first position that is within the <see cref="DesiredAccuracy"/> will be returned. If no
+		/// position matches that accuracy, once <paramref cref="timeout"/> is reached, the most accurate
+		/// position will be returned. If the <paramref name="timeout"/> is reached with no position being acquired, the
+		/// task will be canceled.
+		/// </para>
+		/// <para>
+		/// If this <see cref="IGeolocation"/> currently <see cref="IsListening"/>, the future will be
+		/// set immediately with the last retrieved position.
+		/// </para>
+		/// </remarks>
 		public Task<Position> GetCurrentPosition (int timeout, CancellationToken cancelToken)
 		{
 			if (timeout <= 0 && timeout != Timeout.Infinite)
@@ -99,6 +210,31 @@ namespace Xamarin.Geolocation
 			return tcs.Task;
 		}
 
+		/// <summary>
+		/// Starts listening to position changes with specified thresholds.
+		/// </summary>
+		/// <param name="minTime">A hint for the minimum time between position updates in milliseconds.</param>
+		/// <param name="minDistance">A hint for the minimum distance between position updates in meters.</param>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <para>
+		/// <paramref name="minTime"/> is &lt; 0.
+		/// </para>
+		/// <para>or</para>
+		/// <para>
+		/// <paramref name="minDistance"/> is &lt; 0.
+		/// </para>
+		/// </exception>
+		/// <exception cref="InvalidOperationException">This geolocation already <see cref="IsListening"/>.</exception>
+		/// <seealso cref="StopListening"/>
+		/// <seealso cref="IsListening"/>
+		/// <remarks>
+		/// <para>
+		/// If the underlying OS needs to request location access permissions, it will occur automatically.
+		/// </para>
+		/// <para>
+		/// If a <see cref="PositionError"/> occurs, listening will be halted automatically.
+		/// </para>
+		/// </remarks>
 		public void StartListening (int minTime, double minDistance)
 		{
 			if (minTime < 0)
@@ -117,6 +253,14 @@ namespace Xamarin.Geolocation
 				manager.StartUpdatingHeading ();
 		}
 
+		/// <summary>
+		/// Stops listening to position changes.
+		/// </summary>
+		/// <seealso cref="StartListening"/>
+		/// <remarks>
+		/// If you stop listening before the first position has arrived, and have a pending call to
+		/// <see cref="GetCurrentPosition"/>, it will be canceled.
+		/// </remarks>
 		public void StopListening ()
 		{
 			if (!this.isListening)
