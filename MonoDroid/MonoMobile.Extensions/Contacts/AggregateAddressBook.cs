319bd44948ce4461bf2eed11e9739287923fcc04
ermau <me@ermau.com>
2011-12-01 14:54:55 -0500

    Add AggregateAddressBook

---
+++ MonoDroid/MonoMobile.Extensions/Contacts/AggregateAddressBook.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Android.Content;
+
+namespace Xamarin.Contacts
+{
+	public class AggregateAddressBook
+		: IOrderedQueryable<Contact>
+	{
+		public AggregateAddressBook (Context context)
+		{
+			this.provider = new ContactQueryProvider (false, context.ContentResolver, context.Resources);
+		}
+
+		public IEnumerator<Contact> GetEnumerator()
+		{
+			throw new NotImplementedException();
+			//return ContactHelper.GetContacts(false, this.)
+		}
+
+		private readonly IQueryProvider provider;
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		Type IQueryable.ElementType
+		{
+			get { return typeof (Contact); }
+		}
+
+		Expression IQueryable.Expression
+		{
+			get { return Expression.Constant (this); }
+		}
+
+		IQueryProvider IQueryable.Provider
+		{
+			get { return this.provider; }
+		}
+	}
+}
