07ec4c5ac322f2792aa4955832c82fb0ab8cb838
Keith Ballinger <keithba@gmail.com>
2011-12-07 18:05:43 -0500

    iOS sample

--- MonoTouch/Samples/ContactsSample/DetailContactView.cs
+++ MonoTouch/Samples/ContactsSample/DetailContactView.cs
@@ -3,13 +3,17 @@ using Xamarin.Contacts;
 using MonoTouch.UIKit;
 using System.Text;
 using System.Globalization;
+using System.Drawing;
 
 namespace ContactsSample
 {
 	public class DetailContactView : UIViewController
 	{
 		Contact contact;
-		UILabel outputLabel;
+		UIImageView contactImage;
+		UILabel nameLabel;
+		UILabel phoneLabel;
+		UILabel emailLabel;
 		public DetailContactView (Contact c)
 		{
 			this.contact = c;
@@ -19,33 +23,40 @@ namespace ContactsSample
 		{
 			base.ViewDidLoad ();
 			
-			outputLabel = new UILabel(this.View.Frame);
-			outputLabel.Lines = 0;
-			this.View.AddSubview(outputLabel);
+			View.BackgroundColor = UIColor.White;
 			
-			StringBuilder sb = new StringBuilder();
+			contactImage = new UIImageView(new RectangleF(210, 5, 60, 60));
+			this.View.AddSubview(contactImage);
+			contactImage.Image = contact.PhotoThumbnail;
 			
-			//
-			// contact name
-			//
-			sb.AppendLine(contact.DisplayName);
+			nameLabel = new UILabel(new RectangleF(5, 5, 200, 30));
+			this.View.AddSubview(nameLabel);
+			nameLabel.Text = contact.DisplayName;
 			
+			phoneLabel = new UILabel(new RectangleF(5, 40, 200, 30));
+			this.View.AddSubview(phoneLabel);
+			
+			String phoneString = String.Empty; 
 			foreach(var phone in contact.Phones)
 			{
-				sb.AppendLine(String.Format("{0}: {1}", phone.Label, phone.Number));
+				phoneString = String.Format("{0}: {1}", phone.Label, phone.Number);
+				break; //just take the first phone number in this example
 			}
 			
+			phoneLabel.Text = phoneString;
+			
+			emailLabel = new UILabel(new RectangleF(5, 80, 200, 30));
+			this.View.AddSubview(emailLabel);
+			
+			String emailString = String.Empty; 
 			foreach(var email in contact.Emails)
 			{
-				sb.AppendLine(String.Format("{0}: {1}", email.Label, email.Address));
+				emailString = String.Format("{0}: {1}", email.Label, email.Address);
+				break; //just take the first email in this example
 			}
 			
-			foreach(var organization in contact.Organizations)
-			{
-				sb.AppendLine(String.Format("{0} ({1}): {2}", organization.Name, organization.Label, organization.ContactTitle));
-			}
+			emailLabel.Text = emailString;
 			
-			outputLabel.Text = sb.ToString();
 		}
 	}
 }
