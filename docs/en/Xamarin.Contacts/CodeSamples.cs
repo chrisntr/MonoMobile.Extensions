73cba65c40a2655db6b5bc8eea513d846ecf54b2
ermau <me@ermau.com>
2011-12-08 17:46:21 -0500

    Update Contacts docs

---
+++ docs/en/Xamarin.Contacts/CodeSamples.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Contacts;
+
+public class CodeSamples
+{
+	public CodeSamples()
+	{
+		#region AddressBook
+		var builder = new StringBuilder();
+		var abook = new AddressBook();
+		foreach (Contact c in abook.Where (c => c.FirstName == "Eric" && c.Phones.Any()))
+		{
+			builder.AppendLine (c.DisplayName);
+			foreach (Phone p in c.Phones)
+				builder.AppendLine (String.Format ("{0}: {1}", p.Label, p.Number));
+
+			builder.AppendLine();
+		}
+		#endregion
+	}
+}
