650ee0af8c60e6f96996d23f4bdf4484e1d9cbe0
ermau <me@ermau.com>
2011-12-09 13:20:18 -0500

    Cleanup windows phone, add windows sln

--- WindowsPhone/MonoMobile.Extensions/Properties/AssemblyInfo.cs
+++ WindowsPhone/MonoMobile.Extensions/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MonoMobile.Extensions")]
+[assembly: AssemblyTitle("Xamarin.Mobile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MonoMobile.Extensions")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Xamarin Inc")]
+[assembly: AssemblyProduct("Xamarin.Mobile")]
+[assembly: AssemblyCopyright("Copyright © Xamarin Inc 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
