ff28d5ab77bb2e9bd473ee7f1eaaa70e8a24f6f9
ermau <me@ermau.com>
2011-12-09 21:12:21 -0500

    Add Windows Phone media picker sample

---
+++ WindowsPhone/Samples/MediaPickerSample/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Microsoft.Phone.Controls;
+using Xamarin.Media;
+
+namespace MediaPickerSample
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private MediaPicker picker = new MediaPicker();
+		private async void button1_Click(object sender, RoutedEventArgs e)
+		{
+			Photo photo = await picker.PickPhotoAsync();
+
+		}
+	}
+}
