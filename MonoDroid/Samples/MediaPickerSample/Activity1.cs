e18cd07bc1f0a3426d125c1d7863845b64d1e0d4
ermau <me@ermau.com>
2011-12-12 13:36:17 -0500

    Add Android media picker sample

---
+++ MonoDroid/Samples/MediaPickerSample/Activity1.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Media;
+
+namespace MediaPickerSample
+{
+	[Activity(Label = "MediaPickerSample", MainLauncher = true, Icon = "@drawable/icon")]
+	public class Activity1 : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+			button.Click += delegate
+			{
+				var picker = new MediaPicker (this);
+				picker.PickPhotoAsync()
+					.ContinueWith (t => RunOnUiThread (() =>
+					{
+						button.Text = t.Result.FilePath;
+					}));
+			};
+		}
+	}
+}
+
