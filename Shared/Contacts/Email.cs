6e94460dd9c7dd42d583bef39e03729e724ca4c4
ermau <me@ermau.com>
2012-01-02 17:30:20 -0500

    WP doesn't have System.Net.Mail

--- Shared/Contacts/Email.cs
+++ Shared/Contacts/Email.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Net.Mail;
-
 namespace Xamarin.Contacts
 {
 	public enum EmailType
@@ -29,49 +26,5 @@ namespace Xamarin.Contacts
 			get;
 			set;
 		}
-
-		//public string Address
-		//{
-		//    get
-		//    {
-		//        if (this.address == null)
-		//            return null;
-
-		//        return this.address.ToString();
-		//    }
-
-		//    set
-		//    {
-		//        if (String.IsNullOrWhiteSpace (value))
-		//        {
-		//            this.address = null;
-		//            return;
-		//        }
-
-		//        this.address = new MailAddress (value);
-		//    }
-		//}
-
-		//public override string ToString()
-		//{
-		//    return Address;
-		//}
-
-		//private MailAddress address;
-
-		//public static bool TryParse (string address, out Email email)
-		//{
-		//    email = new Email();
-		//    try
-		//    {
-		//        email.Address = address;
-		//        return true;
-		//    }
-		//    catch (Exception)
-		//    {
-		//        email = null;
-		//        return false;
-		//    }
-		//}
 	}
 }
