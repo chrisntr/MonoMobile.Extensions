6b67e4b98b5358c20aa5545fda1d859dc15fbea2
ermau <me@ermau.com>
2012-01-02 15:42:47 -0500

    Update WP MediaPicker prototype sample

--- WindowsPhone/Samples/MediaPickerSample/MainPage.xaml.cs
+++ WindowsPhone/Samples/MediaPickerSample/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 using Microsoft.Phone.Controls;
 using Xamarin.Media;
 
@@ -11,11 +14,42 @@ namespace MediaPickerSample
 			InitializeComponent();
 		}
 
-		private MediaPicker picker = new MediaPicker();
-		private async void button1_Click(object sender, RoutedEventArgs e)
-		{
-			Photo photo = await picker.PickPhotoAsync();
+		private readonly MediaPicker picker = new MediaPicker();
 
+		private async void Pick_OnClick (object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				MediaFile photo = await picker.PickPhotoAsync();
+
+				var bmp = new BitmapImage();
+				bmp.SetSource (photo.GetStream());
+				this.image.Source = bmp;
+
+				if (photo.CanDelete)
+					photo.Delete();
+			}
+			catch (TaskCanceledException ex)
+			{
+			}
+		}
+
+		private async void Take_OnClick (object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				MediaFile photo = await picker.TakePhotoAsync (new StoreMediaOptions());
+
+				var bmp = new BitmapImage();
+				bmp.SetSource (photo.GetStream());
+				this.image.Source = bmp;
+
+				if (photo.CanDelete)
+					photo.Delete();
+			}
+			catch (TaskCanceledException ex)
+			{
+			}
 		}
 	}
 }
