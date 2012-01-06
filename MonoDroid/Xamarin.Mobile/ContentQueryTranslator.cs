2d1a80f4231c17425070cf0679d2f5688a9a6c6c
ermau <me@ermau.com>
2012-01-06 18:24:45 -0500

    Support .Select() and .Where() in translator

--- MonoDroid/Xamarin.Mobile/ContentQueryTranslator.cs
+++ MonoDroid/Xamarin.Mobile/ContentQueryTranslator.cs
@@ -45,7 +45,7 @@ namespace Xamarin
 
 		public string[] ClauseParameters
 		{
-			get { return (this.parameters.Count > 0) ? this.parameters.ToArray() : null; }
+			get { return (this.arguments.Count > 0) ? this.arguments.ToArray() : null; }
 		}
 
 		public string SortString
@@ -67,7 +67,7 @@ namespace Xamarin
 		private List<Tuple<string, Type>> projections;
 		private List<string> columns;
 		private StringBuilder sortBuilder;
-		private readonly List<string> parameters = new List<string>();
+		private readonly List<string> arguments = new List<string>();
 		private readonly StringBuilder queryBuilder = new StringBuilder();
 
 		protected override Expression VisitMethodCall (MethodCallExpression methodCall)
@@ -82,10 +82,12 @@ namespace Xamarin
 			{
 				if (methodCall.Method.Name == "Where")
 					expression = VisitWhere (methodCall);
-				else if (methodCall.Method.Name == "Any")
-					expression = VisitAny (methodCall);
-				else if (methodCall.Method.Name == "Select" || methodCall.Method.Name == "SelectMany")
+//				else if (methodCall.Method.Name == "Any")
+//					expression = VisitAny (methodCall);
+				else if (methodCall.Method.Name == "Select")
 					expression = VisitSelect (methodCall);
+				else if (methodCall.Method.Name == "SelectMany")
+					expression = VisitSelectMany (methodCall);
 				else if (methodCall.Method.Name == "OrderBy" || methodCall.Method.Name == "OrderByDescending")
 					expression = VisitOrder (methodCall);
 			}
@@ -95,32 +97,161 @@ namespace Xamarin
 
 		private MethodCallExpression VisitAny (MethodCallExpression methodCall)
 		{
-			return methodCall;
+		    return methodCall;
+		}
+
+		private class WhereEvaluator
+			: ExpressionVisitor
+		{
+			public WhereEvaluator (ITableFinder tableFinder, Android.Net.Uri existingTable)
+			{
+				this.tableFinder = tableFinder;
+				this.table = existingTable;
+			}
+
+			public TableFindResult Result
+			{
+				get;
+				private set;
+			}
+
+			public bool Fallback
+			{
+				get;
+				private set;
+			}
+
+			public Expression Evaluate (Expression expression)
+			{
+				expression = Visit (expression);
+
+				Result = new TableFindResult (this.table, this.builder.ToString(), this.arguments);
+
+				return expression;
+			}
+
+			private readonly ITableFinder tableFinder;
+			private Android.Net.Uri table;
+			private readonly StringBuilder builder = new StringBuilder();
+			private readonly List<string> arguments = new List<string>();
+
+			protected override Expression VisitMemberAccess (MemberExpression memberExpression)
+			{
+				TableFindResult result = this.tableFinder.Find (memberExpression);
+				if (this.table == null)
+				{
+					this.table = result.Table;
+					this.builder.Append (result.QueryString);
+					this.arguments.AddRange (result.Arguments);
+				}
+				else if (this.table != result.Table)
+				{
+					Fallback = true;
+					return memberExpression;
+				}
+
+				Tuple<string, Type> column = this.tableFinder.GetColumn (memberExpression.Member);
+				if (column == null)
+				{
+					Fallback = true;
+					return memberExpression;
+				}
+
+				if (this.builder.Length > 0)
+					this.builder.Append (" AND ");
+
+				this.builder.Append (column.Item1);
+
+				return base.VisitMemberAccess (memberExpression);
+			}
+
+			protected override Expression VisitConstant (ConstantExpression constant)
+			{
+				if (constant.Value is IQueryable)
+					return constant;
+
+				if (constant.Value == null)
+					this.builder.Append ("NULL");
+				else
+				{
+					switch (Type.GetTypeCode (constant.Value.GetType()))
+					{
+						case TypeCode.Object:
+							Fallback = true;
+							return constant;
+
+						case TypeCode.Boolean:
+							this.arguments.Add ((bool)constant.Value ? "1" : "0");
+							this.builder.Append ("?");
+							break;
+
+						default:
+							this.arguments.Add (constant.Value.ToString());
+							this.builder.Append ("?");
+							break;
+					}
+				}
+
+				return base.VisitConstant (constant);
+			}
+
+			protected override Expression VisitBinary (BinaryExpression binary)
+			{
+				this.builder.Append ("(");
+
+				Visit (binary.Left);
+
+				switch (binary.NodeType)
+				{
+					case ExpressionType.And:
+						this.builder.Append (" AND ");
+						break;
+
+					case ExpressionType.Or:
+						this.builder.Append (" OR ");
+						break;
+
+					case ExpressionType.Equal:
+						this.builder.Append (" = ");
+						break;
+
+					default:
+						Fallback = true;
+						return binary;
+				}
+
+				Visit (binary.Right);
+
+				this.builder.Append (")");
+
+				return binary;
+			}
 		}
 
 		private Expression VisitWhere (MethodCallExpression methodCall)
 		{
-			// TODO: Need to actually evaluate expression to determine
-			// whether we can use it in the query, the operand
-			// and any constant values
+			Expression expression = ExpressionEvaluator.Evaluate (methodCall);
 
-			//MemberExpression me = FindMemberExpression (methodCall.Arguments[1]);
-			//if (!TryGetTable (me))
-			//    return methodCall;
+			var eval = new WhereEvaluator (this.tableFinder, Table);
+			expression = eval.Evaluate (expression);
 
-			//if (me.Member.DeclaringType == typeof(Contact))
-			//{
-			//    string column = GetContactColumn (me.Member);
-			//    //this.parameters.Add (column);
-				
-			//    if (this.queryBuilder.Length > 0)
-			//        this.queryBuilder.Append (" AND ");
+			TableFindResult result = eval.Result;
+			if (Table == null)
+				Table = result.Table;
 
-			//    this.queryBuilder.Append (column);
-			//    //this.queryBuilder.Append ("=?");
-			//}
+			if (eval.Fallback || result.Table == null || result.Table != Table)
+			{
+				this.fallback = true;
+				return methodCall;
+			}
 
-			return methodCall;
+			this.arguments.AddRange (result.Arguments);
+			if (this.queryBuilder.Length > 0)
+				this.queryBuilder.Append (" AND ");
+
+			this.queryBuilder.Append (result.QueryString);
+
+			return methodCall.Arguments[0];
 		}
 
 		private Expression VisitSelect (MethodCallExpression methodCall)
@@ -140,6 +271,47 @@ namespace Xamarin
 			this.fallback = true;
 
 			return methodCall.Arguments[0];
+		}
+
+//		private Expression VisitSelect (MethodCallExpression methodCall)
+//		{
+//			List<MemberExpression> mes = MemberExpressionFinder.Find (methodCall.Arguments[1], this.tableFinder);
+//			if (!TryGetTable (mes))
+//				return methodCall;
+//
+//			Type returnType = null;
+//
+//			List<Tuple<string, Type>> projs = new List<Tuple<string, Type>>();
+//			foreach (MemberExpression me in mes)
+//			{
+//				Tuple<string, Type> column = this.tableFinder.GetColumn (me.Member);
+//				if (column == null)
+//					return methodCall;
+//				
+//				if (returnType == null)
+//					returnType = column.Item2;
+//				if (returnType != column.Item2)
+//					return methodCall;
+//
+//				projs.Add (column);
+//			}
+//
+//			ReturnType = returnType;
+//			this.fallback = true;
+//
+//			(this.projections ?? (this.projections = new List<Tuple<string, Type>>()))
+//				.AddRange (projs);
+//
+//			return methodCall.Arguments[0];
+//		}
+
+		private Expression VisitSelectMany (MethodCallExpression methodCall)
+		{
+//			List<MemberExpression> mes = MemberExpressionFinder.Find (methodCall, this.tableFinder);
+//			if (!TryGetTable (mes))
+//				return methodCall;
+
+			return methodCall;
 		}
 
 		private Expression VisitOrder (MethodCallExpression methodCall)
@@ -164,6 +336,55 @@ namespace Xamarin
 			}
 
 			return methodCall;
+		}
+
+		private bool TryGetTable (List<MemberExpression> memberExpressions)
+		{
+			if (memberExpressions.Count == 0)
+			{
+				this.fallback = true;
+				return false;
+			}
+
+			Android.Net.Uri existingTable = Table;
+
+			TableFindResult presult = null;
+
+			foreach (MemberExpression me in memberExpressions)
+			{
+				TableFindResult result = this.tableFinder.Find (me);
+				if (result.Table == null)
+				{
+					this.fallback = true;
+					return false;
+				}
+
+				if (existingTable == null)
+				{
+					existingTable = result.Table;
+					presult = result;
+				}
+				else if (existingTable != result.Table)
+				{
+					this.fallback = true;
+					return false;
+				}
+			}
+
+			if (presult == null)
+			{
+				this.fallback = true;
+				return false;
+			}
+
+			Table = presult.Table;
+
+			if (presult.QueryString != null)
+				this.queryBuilder.Append (presult.QueryString);
+
+			this.arguments.AddRange (presult.Arguments);
+
+			return true;
 		}
 
 		private bool TryGetTable (MemberExpression me)
