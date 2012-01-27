ece5eade1e9d44e836d9d7e84dbfb8b6e8126191
ermau <me@ermau.com>
2012-01-27 12:42:53 -0500

    Windows Phone Geolocator

---
+++ WindowsPhone/Xamarin.Mobile/Geolocation/Geolocator.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Device.Location;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xamarin.Geolocation
+{
+	public class Geolocator
+	{
+		public Geolocator()
+		{
+			GeoCoordinateWatcher w = new GeoCoordinateWatcher();
+			IsGeolocationEnabled = (w.Permission == GeoPositionPermission.Granted);
+			w.Dispose();
+		}
+
+		public event EventHandler<PositionErrorEventArgs> PositionError;
+		public event EventHandler<PositionEventArgs> PositionChanged;
+
+		public bool IsGeolocationAvailable
+		{
+			get { return true; }
+		}
+
+		public bool IsGeolocationEnabled
+		{
+			get;
+			private set;
+		}
+
+		public double DesiredAccuracy
+		{
+			get;
+			set;
+		}
+
+		public bool SupportsHeading
+		{
+			get { return true; }
+		}
+
+		public bool IsListening
+		{
+			get { return (this.watcher != null); }
+		}
+
+		public Task<Position> GetPositionAsync (CancellationToken cancelToken)
+		{
+			return GetPositionAsync (Timeout.Infinite, cancelToken);
+		}
+
+		public Task<Position> GetPositionAsync (int timeout)
+		{
+			return GetPositionAsync (timeout, CancellationToken.None);
+		}
+
+		public Task<Position> GetPositionAsync (int timeout, CancellationToken cancelToken)
+		{
+			if (timeout <= 0 && timeout != Timeout.Infinite)
+				throw new ArgumentOutOfRangeException ("timeout", "timeout must be greater than or equal to 0");
+
+			return new SinglePositionListener (DesiredAccuracy, timeout, cancelToken).Task;
+		}
+
+		public void StartListening (int minTime, double minDistance)
+		{
+			if (minTime < 0)
+				throw new ArgumentOutOfRangeException ("minTime");
+			if (minDistance < 0)
+				throw new ArgumentOutOfRangeException ("minDistance");
+			if (IsListening)
+				throw new InvalidOperationException ("This Geolocator is already listening");
+
+			this.watcher = new GeoCoordinateWatcher (GetAccuracy (DesiredAccuracy));
+			this.watcher.MovementThreshold = minDistance;
+			this.watcher.PositionChanged += WatcherOnPositionChanged;
+			this.watcher.StatusChanged += WatcherOnStatusChanged;
+			this.watcher.Start();
+		}
+
+		public void StopListening()
+		{
+			this.watcher.PositionChanged -= WatcherOnPositionChanged;
+			this.watcher.Stop();
+			this.watcher.Dispose();
+			this.watcher = null;
+		}
+
+		private GeoCoordinateWatcher watcher;
+
+		private void WatcherOnStatusChanged (object sender, GeoPositionStatusChangedEventArgs e)
+		{
+			GeolocationError error;
+			switch (e.Status)
+			{
+				case GeoPositionStatus.Disabled:
+					error = GeolocationError.Unauthorized;
+					break;
+
+				case GeoPositionStatus.NoData:
+					error = GeolocationError.PositionUnavailable;
+					break;
+
+				default:
+					return;
+			}
+
+			var perror = PositionError;
+			if (perror != null)
+				perror (this, new PositionErrorEventArgs (error));
+		}
+
+		private void WatcherOnPositionChanged (object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+		{
+			Position p = GetPosition (e.Position);
+			if (p != null)
+			{
+				var pupdate = PositionChanged;
+				if (pupdate != null)
+					pupdate (this, new PositionEventArgs (p));
+			}
+		}
+
+		internal static GeoPositionAccuracy GetAccuracy (double desiredAccuracy)
+		{
+			if (desiredAccuracy < 100)
+				return GeoPositionAccuracy.High;
+
+			return GeoPositionAccuracy.Default;
+		}
+
+		internal static Position GetPosition (GeoPosition<GeoCoordinate> position)
+		{
+			if (position.Location.IsUnknown)
+				return null;
+
+			var p = new Position();
+			p.Accuracy = position.Location.HorizontalAccuracy;
+			p.Longitude = position.Location.Longitude;
+			p.Latitude = position.Location.Latitude;
+
+			p.AltitudeAccuracy = position.Location.VerticalAccuracy;
+			p.Altitude = position.Location.Altitude;
+
+			p.Heading = position.Location.Course;
+
+			p.Speed = position.Location.Speed;
+			p.Timestamp = position.Timestamp;
+			
+			return p;
+		}
+	}
+}
---
+++ WindowsPhone/Xamarin.Mobile/Geolocation/SinglePositionListener.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Device.Location;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xamarin.Geolocation
+{
+	internal class SinglePositionListener
+	{
+		internal SinglePositionListener (double accuracy, int timeout, CancellationToken cancelToken)
+		{
+			cancelToken.Register (HandleTimeout, true);
+
+			this.watcher = new GeoCoordinateWatcher (Geolocator.GetAccuracy (accuracy));
+			this.watcher.PositionChanged += WatcherOnPositionChanged;
+			this.watcher.StatusChanged += WatcherOnStatusChanged;
+
+			this.watcher.Start();
+
+			this.desiredAccuracy = accuracy;
+			if (timeout != Timeout.Infinite)
+				this.timeout = new Timer (HandleTimeout);
+
+			Task.ContinueWith (Cleanup);
+		}
+
+		public Task<Position> Task
+		{
+			get { return this.tcs.Task; }
+		}
+
+		private GeoPosition<GeoCoordinate> bestPosition;
+		private readonly GeoCoordinateWatcher watcher;
+		private readonly double desiredAccuracy;
+		private readonly Timer timeout;
+		private readonly TaskCompletionSource<Position> tcs = new TaskCompletionSource<Position>();
+
+		private void Cleanup (Task task)
+		{
+			if (this.timeout != null)
+				this.timeout.Dispose();
+
+			this.watcher.Stop();
+			this.watcher.Dispose();
+		}
+
+		private void HandleTimeout (object state)
+		{
+			if (state != null && (bool)state)
+				this.tcs.TrySetCanceled();
+
+			if (this.bestPosition != null)
+				this.tcs.TrySetResult (Geolocator.GetPosition (this.bestPosition));
+			else
+				this.tcs.TrySetCanceled();
+		}
+
+		private void WatcherOnStatusChanged (object sender, GeoPositionStatusChangedEventArgs e)
+		{
+			switch (e.Status)
+			{
+				case GeoPositionStatus.NoData:
+					this.tcs.TrySetException (new GeolocationException (GeolocationError.PositionUnavailable));
+					break;
+
+				case GeoPositionStatus.Disabled:
+					this.tcs.TrySetException (new GeolocationException (GeolocationError.Unauthorized));
+					break;
+			}
+		}
+
+		private void WatcherOnPositionChanged (object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+		{
+			if (e.Position.Location.IsUnknown)
+				return;
+
+			if (e.Position.Location.HorizontalAccuracy > this.desiredAccuracy)
+				this.tcs.TrySetResult (Geolocator.GetPosition (e.Position));
+
+			if (this.bestPosition == null || e.Position.Location.HorizontalAccuracy > this.bestPosition.Location.HorizontalAccuracy)
+				this.bestPosition = e.Position;
+		}
+	}
+}
