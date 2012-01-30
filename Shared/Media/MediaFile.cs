0a8c4f965f6259fdefda27849b8397caac9c78cd
ermau <me@ermau.com>
2012-01-30 15:14:49 -0500

    GetStream() and IDisposable on MediaFile

--- Shared/Media/MediaFile.cs
+++ Shared/Media/MediaFile.cs
@@ -1,16 +1,61 @@
+using System;
+using System.IO;
+
 namespace Xamarin.Media
 {
-	public class MediaFile
+	public sealed class MediaFile
+		: IDisposable
 	{
-		internal MediaFile (string path)
+		internal MediaFile (string path, Func<Stream> streamGetter, Action<bool> dispose = null)
 		{
-			Path = path;
+			this.dispose = dispose;
+			this.streamGetter = streamGetter;
+			this.path = path;
 		}
 
 		public string Path
 		{
-			get;
-			private set;
+			get
+			{
+				if (this.isDisposed)
+					throw new ObjectDisposedException (null);
+
+				return this.path;
+			}
+		}
+
+		public Stream GetStream()
+		{
+			if (this.isDisposed)
+				throw new ObjectDisposedException (null);
+
+			return this.streamGetter();
+		}
+
+		public void Dispose()
+		{
+			Dispose (true);
+			GC.SuppressFinalize (this);
+		}
+
+		private bool isDisposed;
+		private readonly Action<bool> dispose;
+		private readonly Func<Stream> streamGetter;
+		private readonly string path;
+		
+		private void Dispose (bool disposing)
+		{
+			if (this.isDisposed)
+				return;
+
+			this.isDisposed = true;
+			if (this.dispose != null)
+				this.dispose (disposing);
+		}
+
+		~MediaFile()
+		{
+			Dispose (false);
 		}
 	}
 }
