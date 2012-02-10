5d75999b80f13407f9a06a6e9a0a2f104376e080
ermau <me@ermau.com>
2012-02-10 12:36:16 -0500

    Remove workaround for .AsQueryable() on MT
    The bug causing this workaround was fixed in a recent MT release.

--- MonoTouch/Xamarin.Mobile/Contacts/AddressBook.cs
+++ MonoTouch/Xamarin.Mobile/Contacts/AddressBook.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 namespace Xamarin.Contacts
 {
 	public class AddressBook
-		: IEnumerable<Contact> //IQueryable<Contact>
+		: IQueryable<Contact>
 	{
 		public AddressBook()
 		{
@@ -70,19 +70,19 @@ namespace Xamarin.Contacts
 			return GetEnumerator();
 		}
 		
-		//Type IQueryable.ElementType
-		//{
-		//    get { return typeof(Contact); }
-		//}
+		Type IQueryable.ElementType
+		{
+		    get { return typeof(Contact); }
+		}
 		
-		//Expression IQueryable.Expression
-		//{
-		//    get { return Expression.Constant (this); }
-		//}
+		Expression IQueryable.Expression
+		{
+		    get { return Expression.Constant (this); }
+		}
 		
-		//IQueryProvider IQueryable.Provider
-		//{
-		//    get { return this.provider; }
-		//}
+		IQueryProvider IQueryable.Provider
+		{
+		    get { return this.provider; }
+		}
 	}
 }
