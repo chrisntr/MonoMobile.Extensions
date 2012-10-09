fab0fb6b9a672de0e42d6402f16cd31a1517f804
ermau <me@ermau.com>
2012-10-09 11:46:53 -0400

    Accept inner exception for GeolocationException

--- Shared/Position.cs
+++ Shared/Position.cs
@@ -115,7 +115,20 @@ namespace Xamarin.Geolocation
 		: Exception
 	{
 		public GeolocationException (GeolocationError error)
+			: base ("A geolocation error occured: " + error)
 		{
+			if (!Enum.IsDefined (typeof (GeolocationError), error))
+				throw new ArgumentException ("error is not a valid GelocationError member", "error");
+
+			Error = error;
+		}
+
+		public GeolocationException (GeolocationError error, Exception innerException)
+			: base ("A geolocation error occured: " + error, innerException)
+		{
+			if (!Enum.IsDefined (typeof (GeolocationError), error))
+				throw new ArgumentException ("error is not a valid GelocationError member", "error");
+
 			Error = error;
 		}
 		
