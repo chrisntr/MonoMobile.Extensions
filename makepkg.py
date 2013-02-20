b41cd3c6c40f0d32310199071cbb8409bf572b83
ermau <me@ermau.com>
2013-02-20 13:30:03 -0500

    --sample-no-manifest the windows samples

--- makepkg.py
+++ makepkg.py
@@ -69,8 +69,8 @@ xpkg_args = [
 	"--library", "winrt:" + W8DLL,
 	"--sample", "Xamarin.Mobile Android Samples. Android samples for the AddressBook, MediaPicker and Geolocator APIs.:MonoDroid/Samples/Xamarin.Mobile.Android.Samples.sln",
 	"--sample", "Xamarin.Mobile iOS Samples. iOS samples for the AddressBook, MediaPicker and Geolocator APIs.:MonoTouch/Samples/Xamarin.Mobile.iOS.Samples.sln",
-	"--sample", "Xamarin.Mobile Windows Phone 7.5 Samples. Windows Phone 7 samples for the AddressBook, MediaPicker and Geolocator APIs.:WindowsPhone/Samples/Xamarin.Mobile.WP.Samples.sln",
-	"--sample", "Xamarin.Mobile Windows Phone 8 Samples. Windows Phone 8 Samples for the AddressBook, MediaPicker and Geolocator APIs.:WindowsPhone8/Samples/Xamarin.Mobile.WP8.Samples.sln",
-	"--sample", "Xamarin.Mobile Windows 8 Samples. Windows 8 Samples for the MediaPicker and Geolocator APIs.:WindowsRT/Samples/Xamarin.Mobile.WinRT.Samples.sln"
+	"--sample-no-manifest", "Xamarin.Mobile Windows Phone 7.5 Samples. Windows Phone 7 samples for the AddressBook, MediaPicker and Geolocator APIs.:WindowsPhone/Samples/Xamarin.Mobile.WP.Samples.sln",
+	"--sample-no-manifest", "Xamarin.Mobile Windows Phone 8 Samples. Windows Phone 8 Samples for the AddressBook, MediaPicker and Geolocator APIs.:WindowsPhone8/Samples/Xamarin.Mobile.WP8.Samples.sln",
+	"--sample-no-manifest", "Xamarin.Mobile Windows 8 Samples. Windows 8 Samples for the MediaPicker and Geolocator APIs.:WindowsRT/Samples/Xamarin.Mobile.WinRT.Samples.sln"
 ]
 subprocess.call (xpkg_args)
