b53dfe2a624c37bcb7cd89547d45c307f9e8f580
ermau <me@ermau.com>
2013-06-05 18:57:19 -0400

    Add missing MediaPickerController

---
+++ MonoTouch/Xamarin.Mobile/Media/MediaPickerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using MonoTouch.UIKit;
+
+namespace Xamarin.Media
+{
+	public sealed class MediaPickerController
+		: UIImagePickerController
+	{
+		internal MediaPickerController (MediaPickerDelegate mpDelegate)
+		{
+			base.Delegate = mpDelegate;
+		}
+
+		public override MonoTouch.Foundation.NSObject Delegate
+		{
+			get { return base.Delegate; }
+			set { throw new NotSupportedException(); }
+		}
+
+		public Task<MediaFile> GetResultAsync()
+		{
+			return ((MediaPickerDelegate)Delegate).Task;
+		}
+	}
+}
